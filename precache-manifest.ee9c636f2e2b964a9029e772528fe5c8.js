20f85a7cd27310f401445fa2f69586248f41b193
Melissa <melissadennis7@hotmail.com>
2021-05-15 08:20:39 +0100

    Updates

---
+++ precache-manifest.ee9c636f2e2b964a9029e772528fe5c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d21d280c3f91ce74b8c5e642dd877a1b",
+    "url": "/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "fe2967bad97d5ccf41cb",
+    "url": "/react-expense-tracker/static/css/main.caf1db2f.chunk.css"
+  },
+  {
+    "revision": "02d65ad6ec437f72acf1",
+    "url": "/react-expense-tracker/static/js/2.070f1458.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-expense-tracker/static/js/2.070f1458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe2967bad97d5ccf41cb",
+    "url": "/react-expense-tracker/static/js/main.b78983fe.chunk.js"
+  },
+  {
+    "revision": "cb46f602e7262cec3ea5",
+    "url": "/react-expense-tracker/static/js/runtime-main.9a2ac18d.js"
+  }
+]);
